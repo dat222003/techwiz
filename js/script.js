696fe5d2b0fdfda423b822877134d9ec42d3fd66
dat222003 <dat.lt.1892@aptechlearning.edu.vn>
2022-08-10 15:57:29 +0700

    check user name first

--- js/script.js
+++ js/script.js
@@ -1,17 +1,26 @@
 
 async function CheckIfUserExist() {
-  const respon = await fetch("data.json"),
+  const respon = await fetch("data/data.json"),
    data = await respon.json(),
    user = data.users.user
-   console.log(user[0])
   const user_input =  document.getElementById("name").value
   if (user_input == "") alert("name must be filled")
-  if (Object.values(user).indexOf(user_input) > -1) {
-    console.log('user existed');
-    return
-  }console.log("user not existed")
-
+  for (let index = 0; index < user.length; index++) {
+    const element = user[index];
+    if (Object.values(element).indexOf(user_input) > -1) {
+          console.log("existed")
+          return true
+        }
+  }
+  console.log("not exist")
+  return false
+  
 }
+
+
+
+
+
 
 
 
