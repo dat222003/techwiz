7e194b422330cc9c0bcc5c58176ace376ffb7a9f
dat222003 <dat.lt.1892@aptechlearning.edu.vn>
2022-08-10 18:55:17 +0700

    js

--- script.js
+++ script.js
@@ -1,26 +1,45 @@
-async function CheckIfUserExist() {
-  const respon = await fetch("data/data.json"),
-   data = await respon.json(),
-   user = data.users.user
-  const user_input =  document.getElementById("name").value
-  if (user_input == "") alert("name must be filled")
-  for (let index = 0; index < user.length; index++) {
-    const element = user[index];
-    if (Object.values(element).indexOf(user_input) > -1) {
-          console.log("existed")
-          return true
-        }
-  }
-  console.log("not exist")
-  return false
-  
+// async function CheckIfUserExist() {
+//   var respon = await fetch("data/data.json"),
+//    data = await respon.json(),
+//    user = data.users.user
+//   var user_input =  document.getElementById("name").value
+//   if (user_input == "") alert("name must be filled")
+//   for (let index = 0; index < user.length; index++) {
+//     var element = user[index];
+//     if (Object.values(element).indexOf(user_input) > -1) {
+//           console.log("existed")
+//           return true
+//     }
+//   }
+//   console.log("not exist")
+//   return false
+
+// }
+
+async function GetData() {
+  let respon = await fetch("data/data.json")
+  let data = await respon.json()
+  return data
 }
 
-// Open mobile navbar list
+async function CheckIfUserExist() {
+  var json_data = await GetData(),
+   user = json_data.users.user,
+   user_input = document.getElementById("name").value
+  if (user_input == "") alert("name must be filled")
+  for (let index = 0; index < user.length; index++) {
+    var element = user[index];
+    if (Object.values(element).indexOf(user_input) > -1) {
+      window.location.replace("index.html");
+    }
+  }
+}
+
+// // Open mobile navbar list
 let mobileNavBtn = document.querySelector(".navbar-mobile_btn");
 let mobileNavList = document.querySelector(".navbar-mobile__list");
 mobileNavBtn.onclick = function(){
-  mobileNavList.classList.toggle("open"); 
+  mobileNavList.classList.toggle("open");
 }
 
 
