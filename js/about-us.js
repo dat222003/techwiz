6ad4909a58f4ed69047c3c2eabc8ebbcee6b00f4
Hoang Anh <anh.dh.1876@aptechlearning.edu.vn>
2022-08-11 12:23:20 +0700

    complete self-health video

---
+++ js/about-us.js
@@ -0,0 +1,12 @@
+//Odometer about us
+setTimeout(function () {
+    odometer1.innerHTML = 2500;
+  }, 100);
+  
+  setTimeout(function () {
+    odometer2.innerHTML = 500;
+  }, 100);
+  
+  setTimeout(function () {
+    odometer3.innerHTML = 7500;
+  }, 100);
--- script.js
+++ script.js
@@ -120,19 +120,6 @@ $("input").on("keydown", function search(e) {
   }
 });
 
-//Odometer about us
-// setTimeout(function () {
-//   odometer1.innerHTML = 2500;
-// }, 100);
-
-// setTimeout(function () {
-//   odometer2.innerHTML = 500;
-// }, 100);
-
-// setTimeout(function () {
-//   odometer3.innerHTML = 7500;
-// }, 100);
-
 
 
 
